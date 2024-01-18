d9f04a572131b9a9d6d98db726bf59af00269b7a
Ганболор Мөнхдарь <23LP0102@G401-CC0110.local>
2024-01-18 12:16:01 +0800

    figma

---
+++ src/components/icons/GitHubIcon.jsx
@@ -0,0 +1,127 @@
+export const GitHubIcon = () => {
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="24" height="24" fill="#F5F5F5" />
+    <g id="Styles &#38; Components">
+      <g id="Components">
+        <g id="Icons" filter="url(#filter0_dd_114_2)">
+          <rect
+            x="-382"
+            y="-285"
+            width="1210"
+            height="1100"
+            fill="white"
+            shape-rendering="crispEdges"
+          />
+          <rect
+            x="-381.5"
+            y="-284.5"
+            width="1209"
+            height="1099"
+            stroke="#D1D5DB"
+            shape-rendering="crispEdges"
+          />
+          <g id="Row" clip-path="url(#clip0_114_2)">
+            <g id="Icon">
+              <rect
+                x="-19.5"
+                y="-28.5"
+                width="767"
+                height="347"
+                rx="4.5"
+                stroke="#9747FF"
+                stroke-dasharray="10 5"
+              />
+              <g id="Name=icon-github, Size=24, Theme Mode=Light">
+                <path
+                  id="Vector"
+                  d="M15 22V18C15.1392 16.7473 14.78 15.4901 14 14.5C17 14.5 20 12.5 20 9C20.08 7.75 19.73 6.52 19 5.5C19.28 4.35 19.28 3.15 19 2C19 2 18 2 16 3.5C13.36 3 10.64 3 8.00004 3.5C6.00004 2 5.00004 2 5.00004 2C4.70004 3.15 4.70004 4.35 5.00004 5.5C4.27191 6.51588 3.91851 7.75279 4.00004 9C4.00004 12.5 7.00004 14.5 10 14.5C9.61004 14.99 9.32004 15.55 9.15004 16.15C8.98004 16.75 8.93004 17.38 9.00004 18V22"
+                  stroke="#4B5563"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  id="Vector_2"
+                  d="M9 18C4.49 20 4 16 2 16"
+                  stroke="#4B5563"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+    <defs>
+      <filter
+        id="filter0_dd_114_2"
+        x="-390"
+        y="-285"
+        width="1226"
+        height="1118"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="10" />
+        <feGaussianBlur stdDeviation="4" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.04 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_114_2"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="effect1_dropShadow_114_2"
+          result="effect2_dropShadow_114_2"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect2_dropShadow_114_2"
+          result="shape"
+        />
+      </filter>
+      <clipPath id="clip0_114_2">
+        <rect
+          width="1050"
+          height="348"
+          fill="white"
+          transform="translate(-302 -29)"
+        />
+      </clipPath>
+    </defs>
+  </svg>;
+};
---
+++ src/components/icons/icon.js
@@ -0,0 +1,5 @@
+export * from "./LightModeIcon";
+export * from "./LocationIcon";
+export * from "./GitHubIcon";
+export * from "./FigmaIcon";
+export * from "./TwitterIcon";
